2f671a99a3e0b52ac4c7c3296fa30744fb49b8f9
WeiLun <wayne90040@gmail.com>
2021-12-31 00:44:05 +0800

    ⚡️ improve second chart refresh with globalkey

--- lib/screens/stock_chart_screen.dart
+++ lib/screens/stock_chart_screen.dart
@@ -3,8 +3,6 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_stock_chart/painters/main_painter.dart';
-import 'package:flutter_stock_chart/models/quote.dart';
 import 'package:flutter_stock_chart/second_painter_type.dart';
 import 'package:flutter_stock_chart/viewmodels/stock_chart_view_model.dart';
 import 'package:flutter_stock_chart/widgets/main_chart_widget.dart';
@@ -27,6 +25,10 @@ class _StockChartScreenState extends State<StockChartScreen> {
   double scale = 1.0;
 
   int typeIndex = 0;
+
+  GlobalKey<SecondChartWidgetState> secondKey = GlobalKey();
+  GlobalKey<SecondChartWidgetState> secondKey2 = GlobalKey();
+
   List<SecondPainterType> types = [
     SecondPainterType.VOL,
     SecondPainterType.MACD,
@@ -47,75 +49,71 @@ class _StockChartScreenState extends State<StockChartScreen> {
     double width = MediaQuery.of(context).size.width;
     double maxScrollX = (viewModel.maxCount - (width ~/ candleSpace)).abs() * candleSpace;
 
-    print("Quote${viewModel.quote}");
-
     return SafeArea(
       child: Container(
+        color: Colors.black,
         child: Flex(
           direction: Axis.vertical,
           children: [
             Expanded(
-                flex: 3,
-                child: GestureDetector(
-                  child: viewModel.quote == null ? Container() :
-                  MainChartWidget(quote: viewModel.quote!, scrollX: scrollX, scale: scale),
+              flex: 3,
+              child: GestureDetector(
+                child: viewModel.quote == null ? Container() :
+                MainChartWidget(quote: viewModel.quote!, scrollX: scrollX, scale: scale),
 
-                  onHorizontalDragStart: (_ ) {
-                    // 水平移動開始
-                    isHorizontalDrag = true;
-                  },
-                  onHorizontalDragUpdate: (detail) {
-                    if (isScaling == true) return;
+                onHorizontalDragStart: (_ ) {
+                  // 水平移動開始
+                  isHorizontalDrag = true;
+                },
+                onHorizontalDragUpdate: (detail) {
+                  if (isScaling == true) return;
 
-                    // 水平移動 更新 Chart
-                    // clamp 用於約束數字的範圍
-                    scrollX = ((detail.primaryDelta ?? 0.0) + scrollX).clamp(0.0, maxScrollX);
-                    setState(() {
+                  // 水平移動 更新 Chart
+                  // clamp 用於約束數字的範圍
+                  scrollX = ((detail.primaryDelta ?? 0.0) + scrollX).clamp(0.0, maxScrollX);
+                  setState(() {
 
-                    });
-                  },
-                  onHorizontalDragEnd: (_ ) {
-                    isHorizontalDrag = false;
-                  },
+                  });
+                },
+                onHorizontalDragEnd: (_ ) {
+                  isHorizontalDrag = false;
+                },
 
-                  onScaleStart: (_ ) {
-                    isScaling = true;
-                  },
-                )
+                onScaleStart: (_ ) {
+                  isScaling = true;
+                },
+              )
             ),
             Expanded(
-                flex: 1,
-                child: GestureDetector(
-                  child: (viewModel.quote == null) ? Container() :
-                  SecondChartWidget(
-                      quote: viewModel.quote!,
-                      scale: scale,
-                      scrollX: scrollX,
-                      types: types,
-                      typeIndex: typeIndex),
-                  onTap: () {
-                    setState(() {
-                      typeIndex = (typeIndex + 1) % types.length;
-                    });
-                  },
-                )
+              flex: 1,
+              child: GestureDetector(
+                child: (viewModel.quote == null) ? Container() :
+                SecondChartWidget(
+                    key: secondKey,
+                    quote: viewModel.quote!,
+                    scale: scale,
+                    scrollX: scrollX,
+                    types: types),
+                onTap: () {
+                  secondKey.currentState?.changeChart();
+                  // typeIndex = (typeIndex + 1) % types.length;
+                },
+              )
             ),
             Expanded(
-                flex: 1,
-                child: GestureDetector(
-                  child: (viewModel.quote == null) ? Container() :
-                  SecondChartWidget(
-                      quote: viewModel.quote!,
-                      scale: scale,
-                      scrollX: scrollX,
-                      types: types,
-                      typeIndex: typeIndex),
-                  onTap: () {
-                    setState(() {
-                      typeIndex = (typeIndex + 1) % types.length;
-                    });
-                  },
-                )
+              flex: 1,
+              child: GestureDetector(
+                child: (viewModel.quote == null) ? Container() :
+                SecondChartWidget(
+                    key: secondKey2,
+                    quote: viewModel.quote!,
+                    scale: scale,
+                    scrollX: scrollX,
+                    types: types),
+                onTap: () {
+                  secondKey2.currentState?.changeChart();
+                },
+              )
             )
           ],
         ),
--- lib/widgets/second_chart_widget.dart
+++ lib/widgets/second_chart_widget.dart
@@ -6,37 +6,51 @@ import '../models/quote.dart';
 import '../painters/second_painter.dart';
 import '../second_painter_type.dart';
 
-class SecondChartWidget extends StatelessWidget {
+class SecondChartWidget extends StatefulWidget {
 
   const SecondChartWidget({
     Key? key,
     required this.quote,
     required this.scale,
     required this.scrollX,
-    required this.types,
-    required this.typeIndex,
+    required this.types
   }) : super(key: key);
 
   final Quote quote;
   final double scale;
   final double scrollX;
   final List<SecondPainterType> types;
-  final int typeIndex;
+
+
+  @override
+  State<SecondChartWidget> createState() => SecondChartWidgetState();
+}
+
+class SecondChartWidgetState extends State<SecondChartWidget> {
+
+  int typeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+
     return Container(
       width: double.infinity,
       margin: EdgeInsets.symmetric(horizontal: 10),
       child: CustomPaint(
         painter: SecondPainter(
-            quote: quote,
-            scale: scale,
-            scrollX: scrollX,
+            quote: widget.quote,
+            scale: widget.scale,
+            scrollX: widget.scrollX,
             isShowBorder: true,
-            type: types[typeIndex]
+            type: widget.types[typeIndex]
         ),
       ),
     );
+  }
+
+  void changeChart() {
+    setState(() {
+      typeIndex = (typeIndex + 1) % widget.types.length;
+    });
   }
 }
